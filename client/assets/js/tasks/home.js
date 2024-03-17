e45cafad4e25ee4ff9037effacd11be674d6fd85
Nathan Imogo <nathan.imogo@outlook.fr>
2024-03-17 11:26:52 +0100

    modified:   client/assets/js/tasks/home.js modified:   client/assets/style/style.css modified:   client/home.html

--- client/assets/js/tasks/home.js
+++ client/assets/js/tasks/home.js
@@ -1,4 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
+const isFavoriteButton = document.querySelector('is-favorite-button');
+const isLittleMenu = document.querySelector('is-little-menu');
+//const HideWhiteButton = document.querySelector('hideMe');
+const hideMe = document.getElementById('#hideMe');
+
+
+hideMe.addEventListener('mouseenter', function() {
+    hideMe.style.display = 'none';
+});
+
+hideMe.addEventListener('mouseleave', function() {
+    hideMe.style.display = 'block';
+});
+
+isLittleMenu.addEventListener('mouseenter', function() {
+    isLittleMenu.style.display = 'block';
+});
+
+isLittleMenu.addEventListener('mouseleave', function() {
+    isLittleMenu.style.display = 'none';
+});
+
+isFavoriteButton.addEventListener('click', function() {
   
-    
 });
